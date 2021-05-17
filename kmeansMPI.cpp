ae271ccfb0b54fca5e4f6d579d50525b49c0951d
Mengyang Zhang <mengyzhang87@gmail.com>
2021-05-16 21:18:45 -0400

    fixed bug: calculate global means correctly

--- kmeansMPI.cpp
+++ kmeansMPI.cpp
@@ -10,7 +10,170 @@
 
 using namespace std;
 
-// typedef vector<double> Data;
+void output(int* cluster, int size);
+void printData(double* data, int size);
+void printVec(int* data, int size);
+void initializeCentroids(double* dataset, double* centroids, int N, int K, int M);
+double getDistance(double* centroid, double* data, int M);
+void newCentroids(double* dataset, int* cluster, double* centroids, int* local_cluster_size, int N, int K, int M);
+void read_csv(const string& filename, double* dataset, int N, int M);
+
+int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+
+	if (argc < 4) {
+		printf("Usage: ./kmeans <# of clusters> <no of data> <dimension>\n");
+		exit(1);
+	}
+
+	int rank, size;
+	MPI_Comm comm = MPI_COMM_WORLD;
+	MPI_Comm_rank(comm, &rank);
+	MPI_Comm_size(comm, &size);
+
+	const int K = atoi(argv[1]);
+	const int ROOT = 0;
+	const int N = atoi(argv[2]);
+	const int M = atoi(argv[3]);
+	int elements_per_proc = N / size;
+
+	// Store data as 1D array
+	double* dataset = NULL;
+
+	// 0. read data from data.csv file
+	if (rank == ROOT) {
+		dataset = (double*) malloc(N * M * sizeof(double));
+		read_csv("data3.csv", dataset, N, M);
+		printf("loaded %lu data with dimension of %lu from %s file\n", N, M, "data.csv");
+	}
+
+	// 1. assign N/P data to each processor
+	
+	// MPI_Datatype MPI_data;
+	// MPI_Type_vector(N, M, M, MPI_DOUBLE, &MPI_data);
+	// MPI_Type_commit(&MPI_data);
+	
+	double* subarray = (double*) malloc(elements_per_proc * M * sizeof(double));
+	int* membership = (int*) malloc(elements_per_proc * sizeof(int));
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+	printf("Rank %d: scatter dataset to each processor\n", rank);
+	MPI_Scatter(dataset, elements_per_proc * M, MPI_DOUBLE, subarray, elements_per_proc * M, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	// 2. Node 0 randomly choose K points as cluster means and broadcast
+	double* local_means = (double*) malloc(K * M * sizeof(double));
+	if (rank == ROOT) initializeCentroids(dataset, local_means, N, K, M);
+	
+	
+	printf("Rank %d: broadcast k means\n", rank);
+	MPI_Barrier(MPI_COMM_WORLD);	
+	MPI_Bcast(local_means, K * M, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	int iter = 0;
+	double changedPercent = 1.0;
+	double* all_local_means = (double*) malloc(size * K * M * sizeof(double));
+	int* local_cluster_size = (int*) malloc(K * sizeof(int)); // key: cluster id, value: # of data
+	int* all_local_cluster_size = (int*) malloc(size * K * sizeof(int));
+
+	while(iter < 10000 && changedPercent > 0.001) {
+		// printf("RANK %d - iter%d - local means: ", rank, iter);
+		// printData(local_means, K * M);
+
+		// 3. for each data point find membership 
+		int memberChanged = 0;
+		for (int i = 0; i < elements_per_proc; ++i) {
+			double minDistance = INT_MAX;
+			int closestCentroid;
+
+			for (int j = 0; j < K; ++j) {
+				//printf("RANK %d - COMPUTE distance between local means %d and subarray %d\n",rank, j, i);
+				//printData(&local_means[j * M], M);
+				//printf("RANK %d\n", rank);
+				//printData(&subarray[i * M], M);
+				double distance = getDistance(&local_means[j * M], &subarray[i * M], M);
+				if (distance < minDistance) {
+					minDistance = distance;
+					closestCentroid = j;
+				}
+			}
+
+			//printf("Rank %d - Data %d - new centroid: %d\n", rank, i, closestCentroid);
+
+			if (closestCentroid != membership[i]) {
+				membership[i] = closestCentroid;
+				memberChanged++;
+			}
+		}
+
+		// 4. Recalculate local means for each cluster in each processor
+		newCentroids(subarray, membership, local_means, local_cluster_size, elements_per_proc, K, M);
+
+		// 5. globally broadcast all local means for each processor to find the global mean
+		MPI_Allgather(local_means, K * M, MPI_DOUBLE, all_local_means, K * M, MPI_DOUBLE, MPI_COMM_WORLD);
+		MPI_Allgather(local_cluster_size, K, MPI_INT, all_local_cluster_size, K, MPI_INT, MPI_COMM_WORLD);
+
+		// calculate global means
+		// 1.1 calculate sum of means from all processors
+		for (int i = 0; i < K * M; ++i) local_means[i] = 0.0; // clear previous local means
+
+		for (int i = 0; i < K; ++i) {
+			for (int j = 0; j < M; ++j) {
+				for (int p = 0; p < size; ++p) {
+					// local mean of cluster i (jth dimension) += local mean of cluster i (jth dimension) on proc p * cluster i sizeon proc p
+					local_means[i * M + j] += all_local_means[p * K * M + i * M + j] * all_local_cluster_size[p * K + i];
+				}
+			}
+		}
+		
+		// 1.2 calculate total size of each cluster
+		for (int i = 0; i < K; ++i) local_cluster_size[i] = 0; // clear so can add
+
+		for (int i = 0; i < K; ++i) {
+			for (int p = 0; p < size; ++p) {
+				local_cluster_size[i] += all_local_cluster_size[p * K + i];
+			}
+		}
+
+		// 1.3 mean = sum / count
+		for (int i = 0; i < K; ++i) {
+			for (int j = 0; j < M; ++j) local_means[i * M + j] /= local_cluster_size[i];
+		}
+
+		changedPercent = (double) memberChanged / elements_per_proc;
+		// printf("RANK %d - iteration %d: %d, %7.3f\n", rank, iter, memberChanged, changedPercent);
+		printf("RANK %d - iteration %d:\n", rank, iter);
+		printData(subarray, elements_per_proc);
+		printVec(membership, elements_per_proc);
+		printData(local_means, K);
+		iter++;
+	}
+
+	// MPI_Barrier(MPI_COMM_WORLD);
+
+	// Gather Cluster Result
+	int* global_membership = NULL;
+	if (rank == ROOT) {
+		global_membership = (int*) malloc(N * sizeof(int));
+	}
+	MPI_Gather(membership, elements_per_proc, MPI_INT, global_membership, elements_per_proc, MPI_INT,
+				ROOT, MPI_COMM_WORLD);
+
+	if (rank == ROOT) output(global_membership, N);
+	
+	free(dataset);
+	free(subarray);
+	free(local_means);
+	free(all_local_means);
+	free(membership);
+	free(global_membership);
+
+	MPI_Finalize();
+
+	return 0;
+}
 
 void output(int* cluster, int size) {
 	for (int i = 0; i < size; ++i) printf("Data %d: cluser %d\n", i, cluster[i]);
@@ -21,13 +184,18 @@ void printData(double* data, int size) {
 	cout << endl;
 }
 
+void printVec(int* data, int size) {
+	for (int i = 0; i < size; ++i) cout << *data++ << " ";
+	cout << endl;
+}
+
 // random select k centroids
 void initializeCentroids(double* dataset, double* centroids, int N, int K, int M) {
-	centroids = (double*) malloc(K * M * sizeof(double));
+	// centroids = (double*) malloc(K * M * sizeof(double));
 	srand(time(0));
 	int index;
 	for (int i = 0; i < K; ++i) {
-		index = rand() % N;
+		index = rand() % N; // randomly choose a data point from dataset
 		for (int j = 0; j < M; ++j) centroids[i * M + j] = dataset[index * M + j];
 	}
 }
@@ -42,20 +210,19 @@ double getDistance(double* centroid, double* data, int M) {
 	return sqrt(squareDistance);
 }
 
-
 // 1. calculate mean of each cluster as new centroid
 // 2. store new centroids
-void newCentroids(double* dataset, const vector<int>& cluster, double* centroids, int N, int K, int M) {
+void newCentroids(double* dataset, int* cluster, double* centroids, int* local_cluster_size, int N, int K, int M) {
 	for (int i = 0; i < K * M; ++i) centroids[i] = 0.0;
+	for (int i = 0; i < K; ++i) local_cluster_size[i] = 0;
 
-	vector<int> clusterSize(K, 0);
 	int clusterId;
 
 	for (int i = 0; i < N; ++i) {// for each data
 		// cluster[i] -> cluster id
 		clusterId = cluster[i];
 		//printf("\t\tcluster id %d\n", clusterId);
-		clusterSize[clusterId]++;
+		local_cluster_size[clusterId]++;
 		for (int j = 0; j < M; ++j) {// for each dimension
 			centroids[clusterId * M + j] += dataset[i * M + j];
 		}
@@ -64,11 +231,11 @@ void newCentroids(double* dataset, const vector<int>& cluster, double* centroids
 	// mean = sum / count
 	// assert(newCentroids.size() == k);
 	for (int i = 0; i < K; ++i) {
-		for (int j = 0; j < M; ++j)
-			centroids[i*M + j] /= clusterSize[i];
+		for (int j = 0; j < M; ++j) {
+			if (local_cluster_size[i] > 0) centroids[i*M + j] /= local_cluster_size[i];
+		}
 	}
 }
-
 
 void read_csv(const string& filename, double* dataset, int N, int M) {
 
@@ -103,137 +270,4 @@ void read_csv(const string& filename, double* dataset, int N, int M) {
 
     // Close file
     myFile.close();
-}
-
-
-int main(int argc, char* argv[]) {
-	MPI_Init(&argc, &argv);
-
-	if (argc < 4) {
-		printf("Usage: ./kmeans <# of clusters> <no of data> <dimension>\n");
-		exit(1);
-	}
-
-	const int K = atoi(argv[1]);
-	const int ROOT = 0;
-
-	int rank, size;
-	MPI_Comm comm = MPI_COMM_WORLD;
-	MPI_Comm_rank(comm, &rank);
-	MPI_Comm_size(comm, &size);
-	
-    const int N = atoi(argv[2]);
-    const int M = atoi(argv[3]);
-    int elements_per_proc = N / size;
-
-	// Store data as 1D array
-	double* dataset;
-
-	// 0. read data from data.csv file
-	if (rank == ROOT) {
-		dataset = (double*) malloc(N * M * sizeof(double));
-		read_csv("data.csv", dataset, N, M);
-		printf("loaded %lu data with dimension of %lu from %s file\n", N, M, "data.csv");
-	}
-
-	// 1. assign N/P data to each processor
-	
-	// MPI_Datatype MPI_data;
-	// MPI_Type_vector(N, M, M, MPI_DOUBLE, &MPI_data);
-	// MPI_Type_commit(&MPI_data);
-	
-	// vector<Data> subarray(elements_per_proc);
-	double* subarray = (double*) malloc(elements_per_proc * M * sizeof(double));
-	vector<int> membership(elements_per_proc, -1);
-	
-	MPI_Barrier(MPI_COMM_WORLD);
-	printf("Rank %d: scatter dataset to each processor\n", rank);
-	MPI_Scatter(dataset, elements_per_proc * M, MPI_DOUBLE, subarray, elements_per_proc * M, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
-
-	// 2. Node 0 randomly choose K points as cluster means and broadcast
-	double* local_means = (double*) malloc(K * M * sizeof(double));
-	if (rank == ROOT) initializeCentroids(subarray, local_means, elements_per_proc, K, M);
-	
-	
-	printf("Rank %d: broadcast k means\n", rank);
-	MPI_Barrier(comm);	
-	MPI_Bcast(local_means, K * M, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
-	MPI_Barrier(comm);
-
-	int iter = 0;
-	double changedPercent = 1.0;
-
-	while(iter < 10000 && changedPercent > 0.001) {
-		// 3. for each data point find membership 
-		int memberChanged = 0;
-		for (int i = 0; i < elements_per_proc; ++i) {
-			double minDistance = INT_MAX;
-			int closestCentroid;
-
-			for (int j = 0; j < K; ++j) {
-				//printf("RANK %d - COMPUTE distance between local means %d and subarray %d\n",rank, j, i);
-				//printData(&local_means[j * M], M);
-				//printf("RANK %d\n", rank);
-				//printData(&subarray[i * M], M);
-				double distance = getDistance(&local_means[j * M], &subarray[i * M], M);
-				if (distance < minDistance) {
-					minDistance = distance;
-					closestCentroid = j;
-				}
-			}
-
-			//printf("Rank %d - Data %d - new centroid: %d\n", rank, i, closestCentroid);
-
-			if (closestCentroid != membership[i]) {
-				membership[i] = closestCentroid;
-				memberChanged++;
-			}
-		}
-
-		// 4. Recalculate local means for each cluster in each processor
-		newCentroids(subarray, membership, local_means, elements_per_proc, K, M);
-
-		// 5. globally broadcast all local means for each processor to find the global mean
-		// vector<Data> all_local_means(size);
-		double* all_local_means = (double*) malloc(size * K * M * sizeof(double));
-		MPI_Allgather(local_means, K * M, MPI_DOUBLE, all_local_means, K * M, MPI_DOUBLE, MPI_COMM_WORLD);
-
-		// calculate global means
-		// 1.1 calculate sum of means from all processors
-		// vector<Data> global_means(K, vector<double>(M, 0.0));
-		for (int i = 0; i < K * M; ++i) local_means[i] = 0.0; // clear previous local means
-
-		
-		for (int i = 0; i < K; ++i) {
-			for (int j = 0; j < M; ++j) {
-				for (int p = 0; p < size; ++p) {
-					local_means[i * M + j] += all_local_means[p * K * M + i * M + j];
-				}
-			}
-		}
-		
-
-		// 1.2 mean = sum of means / number of processors
-		for (int i = 0; i < K; ++i) {
-			for (int j = 0; j < M; ++j) local_means[i * M + j] /= size;
-		}
-
-		changedPercent = (double) memberChanged / elements_per_proc;
-		printf("iteration %d: %d, %7.3f\n", iter, memberChanged, changedPercent);
-		iter++;
-	}
-
-	// Gather Cluster Result
-	int* global_membership;
-	if (rank == ROOT) {
-		global_membership = malloc(N * sizeof(int));
-	}
-	MPI_Gather(&membership[0], elements_per_proc, MPI_INT, global_membership, elements_per_proc, MPI_INT,
-				ROOT, MPI_COMM_WORLD);
-
-	if (rank == ROOT) output(global_membership, N);
-
-	MPI_Finalize();
-
-	return 0;
 }
